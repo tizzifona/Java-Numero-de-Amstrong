1b3d8c2f115345cb540c37c69cddaaab233f8886
tizzifona <tizzifona@gmail.com>
2024-11-12 08:51:49 +0100

    Added AmstrongNumber.java

--- src/main/java/projects/f5/amstrong_java/AmstrongNumber.java
+++ src/main/java/projects/f5/amstrong_java/AmstrongNumber.java
@@ -2,4 +2,18 @@ package projects.f5.amstrong_java;
 
 public class AmstrongNumber {
 
+    public static boolean isArmstrongNumber(int number) {
+        int totalDigits = (int) Math.floor(Math.log10(number)) + 1;
+        int sum = 0;
+        int newNumber = number;
+
+        while (newNumber > 0) {
+            int currentDigit = newNumber % 10;
+            sum += Math.pow(currentDigit, totalDigits);
+            newNumber /= 10;
+        }
+
+        return sum == number;
+    }
+
 }
