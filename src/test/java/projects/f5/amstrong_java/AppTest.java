2ffbc5cbe789fdb9fb2e5f8d50f43f5cbc6e48b8
tizzifona <tizzifona@gmail.com>
2024-11-12 09:27:16 +0100

    Added AppTest.java

--- src/test/java/projects/f5/amstrong_java/AppTest.java
+++ src/test/java/projects/f5/amstrong_java/AppTest.java
@@ -2,15 +2,29 @@ package projects.f5.amstrong_java;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    @Test
+    void testMainOutput() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        App.main(new String[] {});
+
+        String output = outputStream.toString();
+
+        assertThat(output, containsString("371 is an Amstrong number"));
+        assertThat(output, containsString("1634 is an Amstrong number"));
+        assertThat(output, containsString("351 is not an Amstrong number"));
+        assertThat(output, containsString("2015 is not an Amstrong number"));
+    }
+
     @Test
     void testApp() {
         assertEquals(1, 1);
